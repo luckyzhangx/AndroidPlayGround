8e020c60df84b986f252b48d2c613d4c55e9523e
luckyzhangx <zhangxiang@mafengwo.com>
2018-08-27 16:09:00 +0800

    下拉筛选

---
+++ dropdownlist/src/main/java/com/luckyzhangx/practice/dropdownlist/DropDownActivity.kt
@@ -0,0 +1,12 @@
+package com.luckyzhangx.practice.dropdownlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DropDownActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drop_down)
+    }
+}
